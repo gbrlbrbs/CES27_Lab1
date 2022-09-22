b3d83e9513ce6dd1bb597ab06ae542ae32e19c1e
del <m.goncalves.m2018@gmail.com>
2022-09-22 11:15:11 -0300

    variables

--- cmd/process/process.go
+++ cmd/process/process.go
@@ -21,14 +21,14 @@ var CliConn []*net.UDPConn //vetor com conexões para os servidores
 var ServConn *net.UDPConn //conexão do meu servidor (onde recebo
  //mensagens dos outros processos)
 
-var id int //numero identificador do processo
+var request_id int //numero identificador do processo
 var mylogicalClock int
-var estouNaCS bool
-var estouEsperando bool
+var insideCS bool
+var waiting bool
 var received_all_replies bool
 var sharedResource *net.UDPConn
 var queued_request []int
-var lc_requisicao int
+var request_clock int
 var replies_received []int
 
 // function that verifies if a specific process is in the queue
@@ -69,7 +69,7 @@ func queueRequest(process_id int){
 //function to build and send reply messages
 func reply2process(process_id int){
 	str_clock:= strconv.Itoa(mylogicalClock)//convert clock (int) into a string type
-	str_id := strconv.Itoa(id)//convert id (int) into a string type
+	str_id := strconv.Itoa(request_id)//convert id (int) into a string type
 	
 	// concatenate
 	message = concatenate(str_id , str_clock, "reply")
@@ -87,7 +87,7 @@ func reply2process(process_id int){
 func askOtherProcessToUseCS(clock int){
 
 	str_clock := strconv.Itoa(clock)//convert clock (int) into a string type 
-	str_id := strconv.Itoa(id)//convert id (int) into a string type
+	str_id := strconv.Itoa(request_id)//convert id (int) into a string type
 	
 	// concatenate
 	message = concatenate(str_id , str_clock, "request")
@@ -105,8 +105,8 @@ func askOtherProcessToUseCS(clock int){
 //function to send message to CS and sleep (all other processes have replied)
 func sendMessageToCS(request_clock int, text string){
 	insideCS = true
-	str_request_clock := strconv.Itoa(lc_requisicao)//convert clock (int) into a string type 
-	str_id := strconv.Itoa(id)//convert id (int) into a string type
+	str_clock := strconv.Itoa(request_clock)//convert clock (int) into a string type 
+	str_id := strconv.Itoa(request_id)//convert id (int) into a string type
 	
 	// concatenate
 	message = concatenate(str_id , str_clock, text)
@@ -123,14 +123,14 @@ func sendMessageToCS(request_clock int, text string){
 
 func replyQueuedRequests(){
 	str_clock := strconv.Itoa(mylogicalClock)//convert clock (int) into a string type 
-	str_id := strconv.Itoa(id)//convert id (int) into a string type
+	str_id := strconv.Itoa(request_id)//convert id (int) into a string type
 	
 	// concatenate
 	message = concatenate(str_id , str_clock, "reply")
 
 	//Reply to all queued processes
-	for _,id:= range queued_request {
-		index := id -1
+	for _,request_id:= range queued_request {
+		index := request_id -1
 		//reply queued request
 		_,err := CliConn[index].Write(buf)
 		if err != nil {
@@ -152,7 +152,7 @@ func releaseCS(){
 	replies_received = nil
 }
 
-func Ricart_Agrawala(lc_requisicao int, text_simples string){
+func Ricart_Agrawala(request_clock int, text_simples string){
 
 	waiting = true
 	askOtherProcessToUseCS(request_clock)
@@ -173,6 +173,63 @@ func Ricart_Agrawala(lc_requisicao int, text_simples string){
 	fmt.Println("Got out of CS!")
 }
 
-func main() {
-	
+func main(){
+	initConnections()
+	insideCS = false
+	waiting = false
+	//O fechamento de conexões deve ficar aqui, assim só fecha
+	//conexão quando a main morrer
+
+	defer ServConn.Close()
+	for i := 0; i < nServers; i++ {
+		defer CliConn[i].Close()
+	}
+
+	//Todo Process fará a mesma coisa: ouvir msg e mandar infinitos
+	//i’s para os outros processos
+	ch := make(chan string)
+	go readInput(ch)
+	for {
+			
+		//Server
+		go doServerJob()
+
+		// When there is a request (from stdin). Do it!
+		select {
+			case x, valid := <- ch :
+				if valid {
+                        compare,_ := strconv.Atoi(x)
+						if ( compare != id && x == "x"){
+
+							//Ver se esta na CS ou esperando
+							if (insideCS || waiting){
+								fmt.Println("x ignorado!")
+							} else {
+								fmt.Printf("Solicitando acesso com ID = %d e Logical Clock = %d\n", request_id, mylogicalClock)
+								text := "CS sugou"
+								request_clock = mylogicalClock
+								go Ricart_Agrawala(request_clock,text)
+							}
+							
+						} else{
+
+							mylogicalClock++
+							fmt.Printf("Atualizado logicalClock para %d \n",mylogicalClock)
+						}
+				} else {
+						 
+					fmt.Println("Channel closed!")
+						
+				}
+				
+			default:
+			
+				// Do nothing in the non-blocking approach.
+			
+				time.Sleep(time.Second * 1)
+		}
+			
+		// Wait a while
+		time.Sleep(time.Second * 1)
+	}
 }
