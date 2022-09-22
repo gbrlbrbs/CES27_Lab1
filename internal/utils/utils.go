42dd364ec8d55a66a5e1e650b1ac36e9d0605a8b
del <m.goncalves.m2018@gmail.com>
2022-09-22 11:28:58 -0300

    capitalizing functions

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -16,26 +16,17 @@ func CheckError(e error) {
 }
 
 //auxiliary function to determine the bigger between two numbers
-func maxNumber(x1, x2 int) int {
+func MaxNumber(x1, x2 int) int {
 	if x1 > x2 {
 		return x1
 	}
 	return x2
 }
 
-//auxiliary function to concatenate ID, clock and 
-func concatenate(str_id string, str_clock string, text string) string {
-	message :=  str_id + "," + str_clock + "," + text
+//auxiliary function to concatenate ID, clock and
+func Concatenate(str_id string, str_clock string, text string) string {
+	message := str_id + "," + str_clock + "," + text
 	buf := []byte(message)
 
 	return message
-}
-
-// auxiliary non-blocking async routine to listen for terminal input
-func readInput(ch chan string) {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		text, _, _ := reader.ReadLine()
-		ch <- string(text)
-	}
 }
