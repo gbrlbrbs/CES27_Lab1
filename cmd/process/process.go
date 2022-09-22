288e6bc9a43bbf9915eecc6c61de336259a13227
gbrlbrbs <gabrielbarbosamartinz_@outlook.com>
2022-09-22 15:52:55 -0300

    finished hopefully

--- cmd/process/process.go
+++ cmd/process/process.go
@@ -1,38 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
-	"strconv"
-	"time"
-	"bufio"
 	"os"
+	"strconv"
+	"strings"
+	"time"
+	"github.com/gbrlbrbs/CES27_Lab1/internal/types"
 	"github.com/gbrlbrbs/CES27_Lab1/internal/utils"
 )
 
-//Variáveis globais interessantes para o processo
-var err string
-var myPort string          //porta do meu servidor
-var nServers int           //qtde de outros processo
-var CliConn []*net.UDPConn //vetor com conexões para os servidores
-//dos outros processos
-var ServConn *net.UDPConn //conexão do meu servidor (onde recebo
-//mensagens dos outros processos)
-
-var request_id int //numero identificador do processo
-var mylogicalClock int
+// global variables
+var safeLogicalClock types.SafeLogicalClock
 var insideCS bool
 var waiting bool
-var received_all_replies bool
-var sharedResource *net.UDPConn
-var queued_request []int
-var request_clock int
-var replies_received []int
+var receivedAllReplies bool
+var repliesReceived []int
+var queuedRequests []int
 
 // function that verifies if a specific process is in the queue
-func searchInList(process_id int) bool {
-	for _, i := range replies_received {
-		if i == process_id {
+func searchInList(processID int, repliesReceived []int) bool {
+	for _, i := range repliesReceived {
+		if i == processID {
 			return true
 		}
 	}
@@ -40,17 +31,17 @@ func searchInList(process_id int) bool {
 }
 
 //function that verifies between two process which one has priority to acess CS
-func amIthepriority(process_id int, process_clock int) bool {
+func amIPriority(processID, processClock, thisID, thisClock int) bool {
 
-	if request_clock < process_clock {//my logical clock at request is lower than another's
-		return true//I am priority
-	} else if process_clock > request_clock {//my logical clock at request isn't lower than another's
-		return false//I'm not priority 
-	} else {//my logical clock at request is equal to another's
-		if request_id < process_id {//my id is lower
-			return true//I am priority
-		} else {//my id isn't lower
-			return false//I'm not priority
+	if thisClock < processClock { 
+		return true 
+	} else if thisClock > processClock { 
+		return false  
+	} else { 
+		if thisID < processID { 
+			return true 
+		} else { 
+			return false 
 		}
 	}
 }
@@ -66,81 +57,73 @@ func readInputFromStdin(ch chan string) {
 	}
 }
 
-//function that creates the queue os processes that have request CS qhile CS was occupied
-//It queues request from requesting process without replying
-func queueRequest(process_id int) {
-	queued_request = append(queued_request, process_id)
+// function that creates the queue os processes that have request CS qhile CS was occupied
+// It queues request from requesting process without replying
+func queueRequest(processID int, queuedRequests []int) (newQueue []int){
+	newQueue = append(queuedRequests, processID)
+	return
 }
 
-//function to build and send reply messages
-func reply2process(process_id int) {
-	str_clock := strconv.Itoa(mylogicalClock) //convert clock (int) into a string type
-	str_id := strconv.Itoa(request_id)        //convert id (int) into a string type
+// function to build and send reply messages
+func replyToProcess(thisClock, processID int, connections []*net.UDPConn) {
+	// convert to string
+	str_clock := strconv.Itoa(thisClock)
+	str_id := strconv.Itoa(processID)
 
 	// utils.Concatenate
-	message := utils.Concatenate(str_id, str_clock, "reply")
-	buf := []byte(message)
+	message, buf := utils.Concatenate(str_id, str_clock, "reply")
 
 	// send reply to process
-	index := process_id - 1
-	_, err := CliConn[index].Write(buf)
-	if err != nil {
-		fmt.Println(message, err)
-	}
+	index := processID - 1
+	_, err := connections[index].Write(buf)
+	utils.PrintErrorAndMessage(err, message)
 }
 
 // function to send to all the other processes a request to use CS
-func askOtherProcessToUseCS(clock int) {
+func askOtherProcessToUseCS(thisClock, thisID int, connections []*net.UDPConn) {
 
-	str_clock := strconv.Itoa(clock)   //convert clock (int) into a string type
-	str_id := strconv.Itoa(request_id) //convert id (int) into a string type
+	str_clock := strconv.Itoa(thisClock)   
+	str_id := strconv.Itoa(thisID) 
 
 	// utils.Concatenate
-	message := utils.Concatenate(str_id, str_clock, "request")
-	buf := []byte(message)
+	message, buf := utils.Concatenate(str_id, str_clock, "request")
 
 	//Multicast request to all N-1 processes
-	for _, conn2process := range CliConn {
+	for _, conn2process := range connections {
 
 		_, err := conn2process.Write(buf)
-		if err != nil {
-			fmt.Println(message, err)
-		}
+		utils.PrintErrorAndMessage(err, message)
 	}
 }
 
-//function to send message to CS and sleep (all other processes have replied)
-func sendMessageToCS(request_clock int, text string) {
+// function to send message to CS and sleep (all other processes have replied)
+func sendMessageToCS(thisClock, thisID int, text string, sharedResource *net.UDPConn) {
 	insideCS = true
-	str_clock := strconv.Itoa(request_clock) //convert clock (int) into a string type
-	str_id := strconv.Itoa(request_id)       //convert id (int) into a string type
+	str_clock := strconv.Itoa(thisClock)
+	str_id := strconv.Itoa(thisID)
 
 	// utils.Concatenate
-	message := utils.Concatenate(str_id, str_clock, text)
-	buf := []byte(message)
+	message, buf := utils.Concatenate(str_id, str_clock, text)
 
 	//send message to shared resource
 	_, err := sharedResource.Write(buf)
-	if err != nil {
-		fmt.Println(message, err)
-	}
+	utils.PrintErrorAndMessage(err, message)
 	//sleep
 	time.Sleep(time.Second * 3)
 }
 
-func replyQueuedRequests() {
-	str_clock := strconv.Itoa(mylogicalClock) //convert clock (int) into a string type
-	str_id := strconv.Itoa(request_id)        //convert id (int) into a string type
+func replyQueuedRequests(thisClock, thisID int, connections []*net.UDPConn, queuedRequests []int) {
+	str_clock := strconv.Itoa(thisClock)
+	str_id := strconv.Itoa(thisID)
 
 	// utils.Concatenate
-	message := utils.Concatenate(str_id, str_clock, "reply")
-	buf := []byte(message)
+	message, buf := utils.Concatenate(str_id, str_clock, "reply")
 
 	//Reply to all queued processes
-	for _, request_id := range queued_request {
+	for _, request_id := range queuedRequests {
 		index := request_id - 1
 		//reply queued request
-		_, err := CliConn[index].Write(buf)
+		_, err := connections[index].Write(buf)
 		if err != nil {
 			fmt.Println(message, err)
 		}
@@ -148,38 +131,80 @@ func replyQueuedRequests() {
 }
 
 // function to do the actions of a process leaving CS: change state to "released" and reset the flags
-func releaseCS() {
+func releaseCS(thisClock, thisID int, connections []*net.UDPConn, queuedRequests []int) {
 	insideCS = false
 	waiting = false
-	received_all_replies = false
+	receivedAllReplies = false
 
 	//reply to any queued request
-	replyQueuedRequests()
+	replyQueuedRequests(thisClock, thisID, connections, queuedRequests)
 
 	//clear reply received list
-	replies_received = nil
+	repliesReceived = nil
 }
 
-func Ricart_Agrawala(request_clock int, text string) {
+func RicartAgrawala(thisClock, thisID int, text string, connections []*net.UDPConn, sharedResource *net.UDPConn, queuedRequests []int) {
 
 	waiting = true
-	askOtherProcessToUseCS(request_clock)
+	askOtherProcessToUseCS(thisClock, thisID, connections)
 
 	//Wait until received N-1 replies
 	fmt.Println("I am waiting for all N-1 replies")
-	for !received_all_replies {
-	}
+	for !receivedAllReplies {}
 
 	// Enter CS after receive all N-1 replies
 	fmt.Println("Got inside CS")
 
 	// Send your message to CS
-	sendMessageToCS(request_clock, text)
+	sendMessageToCS(thisClock, thisID, text, sharedResource)
 	fmt.Println("Just sent my message to CS")
 
 	// Leave CS
-	releaseCS()
+	releaseCS(thisClock, thisID, connections, queuedRequests)
 	fmt.Println("Got out of CS!")
+}
+
+func readFromUDP(thisID, nServers int, serverConn *net.UDPConn, connections []*net.UDPConn) {
+	buf := make([]byte, 1024)
+	for {
+		n, _, err := serverConn.ReadFromUDP(buf)
+		utils.PrintError(err)
+		// aux == "<id>,<clock>,<type>"
+		messageStr := string(buf[:n])
+		// messageArr == ["<id>", "<clock>", "<type>"]
+		messageArr := strings.Split(messageStr, ",")
+		processIDStr := messageArr[0]
+		processClockStr := messageArr[1]
+		messageType := messageArr[2]
+		fmt.Println("Received message from process", processIDStr, "having logical clock =", processClockStr, "of type =", messageType)
+		processID, _ := strconv.Atoi(processIDStr)
+		processClock, _ := strconv.Atoi(processClockStr)
+		thisClock := safeLogicalClock.Get()
+		if messageType == "request" {
+			if insideCS || (waiting && amIPriority(processID, processClock, thisID, thisClock)) {
+				// using or waiting and has the priority
+				fmt.Println("queued request from process", processIDStr)
+				queuedRequests = queueRequest(processID, queuedRequests)
+			} else { 
+				// reply immediately
+				fmt.Println("replying process", processIDStr)
+				replyToProcess(thisClock, processID, connections)
+			}
+		} else if messageType == "reply" {
+			if !searchInList(processID, repliesReceived) {
+				// append to replies received if not found
+				repliesReceived = append(repliesReceived, processID)
+			} 
+			if len(repliesReceived) >= nServers {
+				fmt.Println("received all replies")
+				receivedAllReplies = true
+			}
+		} else {
+			fmt.Println("unknown message type; message =", messageStr, "type =", messageType)
+		}
+		safeLogicalClock.Set(utils.MaxNumber(safeLogicalClock.Get(), processClock) + 1)
+		fmt.Println("logical clock now set to", safeLogicalClock.Get())
+	}
 }
 
 func processListenUDP(procPort string) (serverConn *net.UDPConn) {
@@ -213,6 +238,7 @@ func makeConnections(nServers int, ports []string) (connections []*net.UDPConn, 
 	utils.PrintError(err)
 
 	sharedResource, err = net.DialUDP("udp", localAddr, csAddr)
+	utils.PrintError(err)
 	return
 }
 
@@ -229,7 +255,7 @@ func main() {
 	id, procPort, ports, nServers := getArgs()
 	connections, sharedResource := makeConnections(nServers, ports)
 	serverConn := processListenUDP(procPort)
-	logicalClock := 0
+	safeLogicalClock = types.SafeLogicalClock{} // will create with logicalClock: 0
 
 	// close connections on main function finish
 	defer serverConn.Close()
@@ -240,6 +266,29 @@ func main() {
 	ch := make(chan string)
 	go readInputFromStdin(ch)
 	for {
-		
+		go readFromUDP(id, nServers, serverConn, connections)
+		select {
+			case x, valid := <-ch:
+				if valid {
+					compare, _ := strconv.Atoi(x)
+					if compare != id && x == "x" {
+						if insideCS || waiting {
+							fmt.Println("ignored")
+						} else {
+							thisClock := safeLogicalClock.Get()
+							fmt.Println("requesting access with id =", id, "and logical clock =", thisClock)
+							go RicartAgrawala(thisClock, id, "sugou", connections, sharedResource, queuedRequests)
+						}
+					} else {
+						safeLogicalClock.Increase()
+						fmt.Println("logical clock increased to", safeLogicalClock.Get())
+					}
+				} else {
+					fmt.Println("channel closed")
+				}
+			default:
+				time.Sleep(time.Second * 1)
+		}
+		time.Sleep(time.Second * 1)
 	}
 }
